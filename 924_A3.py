2b25c0e6ea6ef8975a03b71ec36e09a8799d6568
PipoPoopy <141863400+PipoPoopy@users.noreply.github.com>
2023-08-09 23:21:07 +0530

    Add files via upload

---
+++ 924_A3.py
@@ -0,0 +1,49 @@
+dict1 = { "k2": [5, 18, 16, 18, 18], 
+        "k1": [17, 18, 5, 17, 17, 16], 
+        "k3": [17, 18, 16, 5, 9] 
+        }
+
+sort_keys = {}
+keys = sorted(dict1.keys())
+for key in keys:
+    sort_keys[key] = dict1[key]
+
+
+
+avgs = []
+for values in sort_keys.values():
+    average = sum(values) / len(values)
+    avgs.append(average)
+median = sorted(avgs)[len(avgs) // 2]
+
+
+
+highdiff = 0
+maxdis = ""
+for key, values in sort_keys.items():
+    difvalues = set(values)
+    difcount = len(difvalues)
+    if difcount > highdiff:
+        highdiff = difcount
+        maxdis = key
+
+
+N=16
+for key in dict1:
+    values = dict1[key]
+    new_values = []
+    for value in values:
+        if value <= N:
+            new_values.append(value)
+    dict1[key] = new_values
+
+
+print()
+print(sort_keys)
+print()
+print("median=",median)
+print()
+print("key with max distint value=",maxdis)
+print()
+print("After Deletion:", dict1)
+print()
